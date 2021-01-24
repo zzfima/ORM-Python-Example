67af5b53368fc22ae8fb17c0b0a7f6d7a77de34d
zfima <zzfima@gmail.com>
2021-01-24 14:46:43 +0200

    SP

--- main.py
+++ main.py
@@ -15,9 +15,14 @@ for x in my_result:
 print("SELECT")
 my_cursor.execute("SELECT * FROM category")
 my_result = my_cursor.fetchall()
-
 for x in my_result:
     print(x)
+
+# SP
+print('Store procedure')
+my_cursor.callproc("store.new_procedure")
+for result in my_cursor.stored_results():
+    print(result.fetchall())
 
 # ORM
 engine = db.create_engine('mysql+pymysql://root:Aa123456@localhost/store-medium')
@@ -30,3 +35,11 @@ query = db.select([category])
 ResultProxy = connection.execute(query)
 ResultSet = ResultProxy.fetchall()
 print(ResultSet)
+
+# SP
+print('Store procedure')
+connection = engine.raw_connection()
+cursor = connection.cursor()
+cursor.callproc("store.new_procedure")
+results = list(cursor.fetchall())
+print(results)
